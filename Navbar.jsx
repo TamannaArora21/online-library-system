fea46850daea1806e82bcf526f241b8ff8dc3dc9
Tamanna <tamannajogi28@gmail.com>
2026-06-03 19:14:00 +0530

    Create navigation bar component

--- Navbar.jsx
+++ Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <h2>Online Library</h2>
+      <h2>Online Library System</h2>
 
       <Link to="/">Home</Link> |{" "}
       <Link to="/books">Browse Books</Link> |{" "}
